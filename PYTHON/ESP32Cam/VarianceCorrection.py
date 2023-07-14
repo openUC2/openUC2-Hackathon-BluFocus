925bdfa260364009d51332a737b5b8813c3adb66
Rudi <k.r.siebert@icloud.com>
2023-07-14 19:14:49 +0200

    variance function

--- PYTHON/ESP32Cam/VarianceCorrection.py
+++ PYTHON/ESP32Cam/VarianceCorrection.py
@@ -1,7 +1,16 @@
 import numpy as np
-alpha = 30
 
-def coordinate_transformation(x, y):
+
+def variance(frame):
     xprime = np.cos(alpha)*x - np.sin(alpha)*y
     yprime = np.sin(alpha)*x + np.cos(alpha)*y
-    return xprime, yprime
+
+    intensitySum = np.sum(frame)    
+
+    median_x = np.sum(xprime*frame)/intensitySum
+    median_y = np.sum(yprime*frame)/intensitySum
+
+    varriance_x = np.sum((xprime**2)*frame)/intensitySum - median_x**2
+    varriance_y = np.sum((yprime**2)*frame)/intensitySum - median_y**2
+
+    return varriance_x, varriance_y
